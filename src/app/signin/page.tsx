55cc794bd4f587340b5673ea90bf84613e61b7cd
hemanth5055 <kanalahemanthrdy@gmail.com>
2025-08-24 16:42:06 +0530

    Added metadata to signin page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -2,7 +2,10 @@ import Signin from "@/Components/Signin";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
-
+export const metadata = {
+  title: "Sign In - ReadLess",
+  description: "Sign in to ReadLess to keep your PDS concise and organized.",
+};
 const Page = async () => {
   const { userId } = await auth();
   if (userId) redirect("/");
