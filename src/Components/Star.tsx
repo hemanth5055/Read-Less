75d824247586beaa5096082e7c06af7fb4d459dc
hemanth5055 <kanalahemanthrdy@gmail.com>
2025-08-24 16:07:08 +0530

    Updated readme

--- src/Components/Star.tsx
+++ src/Components/Star.tsx
@@ -5,7 +5,7 @@ const GithubButton = () => {
   return (
     <div className="fixed bottom-2 left-4 max-sm:hidden ">
       <a
-        href="https://github.com/your-repo"
+        href="https://github.com/hemanth5055/Read-Less"
         target="_blank"
         rel="noopener noreferrer"
         className="flex items-center gap-2 p-4 rounded-full"
